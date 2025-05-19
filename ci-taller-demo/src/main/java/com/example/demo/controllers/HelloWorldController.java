865d2d2b4922ac0637750d7b0acc68948ba038bf
ulisespallares888 <ulisespallares8@gmail.com>
2025-05-19 15:49:06 -0300

    Agregue status code en el endpoint

--- ci-taller-demo/src/main/java/com/example/demo/controllers/HelloWorldController.java
+++ ci-taller-demo/src/main/java/com/example/demo/controllers/HelloWorldController.java
@@ -1,12 +1,15 @@
 package com.example.demo.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
     @GetMapping("/hello")
+    @ResponseStatus(HttpStatus.OK)
     public String hello() {
         return "Hello world";
     }
