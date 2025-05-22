ab19a746df9fe49d64d72d7939a34af6a3d920f1
ulisespallares888 <ulisespallares8@gmail.com>
2025-05-22 20:38:58 -0300

    Prueba del sonarqube

--- ci-taller-demo/src/main/java/com/example/demo/DemoApplication.java
+++ ci-taller-demo/src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class DemoApplication {
 
+    // Comentario para probar el sonarqube
+
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
