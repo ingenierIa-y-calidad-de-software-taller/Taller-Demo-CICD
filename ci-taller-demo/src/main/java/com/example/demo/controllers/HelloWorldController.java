918df7996483c9fc9f405873c848b06acccdcccf
ulisespallares888 <ulisespallares8@gmail.com>
2025-06-08 02:52:25 -0300

    Método get corregido

--- ci-taller-demo/src/main/java/com/example/demo/controllers/HelloWorldController.java
+++ ci-taller-demo/src/main/java/com/example/demo/controllers/HelloWorldController.java
@@ -8,7 +8,7 @@ public class HelloWorldController {
 
     // GET: Obtener el mensaje
     @GetMapping("/hello")
-    public ResponseEntity<String> Get() {
+    public ResponseEntity<String> get() {
         return ResponseEntity.ok("HelloWorld");
     }
 }
