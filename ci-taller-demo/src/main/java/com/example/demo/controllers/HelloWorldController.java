a52907ee66267b70ee41fe1d892bafcbf811a9df
Ulises Pallarés <54750620+ulisespallares888@users.noreply.github.com>
2025-06-07 23:15:00 -0300

    Update HelloWorldController.java

--- ci-taller-demo/src/main/java/com/example/demo/controllers/HelloWorldController.java
+++ ci-taller-demo/src/main/java/com/example/demo/controllers/HelloWorldController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/hello") // Prefijo común para todos los endpoints
 public class HelloWorldController {
 
-    private String message = "Hello world1";
+    private String message = "Hello world";
 
     // GET: Obtener el mensaje
     @GetMapping
