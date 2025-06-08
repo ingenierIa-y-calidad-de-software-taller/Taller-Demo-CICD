a80dd83c3116c293a6148777d7a936872a7e3a73
Ulises Pallarés <54750620+ulisespallares888@users.noreply.github.com>
2025-06-08 02:19:16 -0300

    Update HelloWorldController.java

--- ci-taller-demo/src/main/java/com/example/demo/controllers/HelloWorldController.java
+++ ci-taller-demo/src/main/java/com/example/demo/controllers/HelloWorldController.java
@@ -9,6 +9,6 @@ public class HelloWorldController {
     // GET: Obtener el mensaje
     @GetMapping("/hello")
     public ResponseEntity<String> Get() {
-        return ResponseEntity.ok("HelloWorld");
+        return ResponseEntity.ok("HelloWorld1");
     }
 }
